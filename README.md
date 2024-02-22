beda499fb05891a577b2d9c1afc6f0590f487176
637211 <alexlaw528@gmail.com>
2024-02-22 14:48:29 -0800

    set up working example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PowerBI-embed-example
-Example code for embedding MS PowerBI into frontend
+# Embed PowerBI into React
+
+https://learn.microsoft.com/en-us/javascript/api/overview/powerbi/powerbi-client-react
+
+## Embed Procedure
+
+- Requires
+  - embed url
+    - report cannot be in "myworkspace". Create a new workspace and move or generate report inside of it
+  - access token
+    - access token can be retrieved within web dev tools (chrome)
+    - From your published report run `copy(powerBIAccessToken)` to retrieve access token
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PowerBI Embed Test</h1>
+      <PowerBIEmbed
+        embedConfig={{
+          type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+          id: process.env.REPORT_ID,
+          embedUrl: process.env.EMBED_URL,
+          accessToken: process.env.ACCESS_TOKEN,
+          tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
+              },
+            },
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+            ["visualClicked", () => console.log("visual clicked")],
+            ["pageChanged", (event) => console.log(event)],
+          ])
+        }
+        cssClassName={"embed-container"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
